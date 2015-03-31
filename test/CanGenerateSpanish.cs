fd13057a289f450aee0eb0492d7753068e23a26d
fschwiet <fschwiet@gmail.com>
2015-03-30 20:24:10 -0500

    can change verbs a bit

--- test/CanGenerateSpanish.cs
+++ test/CanGenerateSpanish.cs
@@ -9,14 +9,18 @@ namespace test
 	{
 		[Test]
 		[NUnit.Framework.TestCase("yo voy")]
-		[NUnit.Framework.TestCase("tú vas")]
+		[NUnit.Framework.TestCase("tú vas")]		
+		[NUnit.Framework.TestCase("usted va")]
 		[NUnit.Framework.TestCase("él va")]
 		[NUnit.Framework.TestCase("ella va")]
-		[NUnit.Framework.TestCase("usted va")]
+
+		[NUnit.Framework.TestCase("nosotros vamos")]
 		[NUnit.Framework.TestCase("vosotros vais")]
+		[NUnit.Framework.TestCase("ustedes van")]
 		[NUnit.Framework.TestCase("ellos van")]
 		[NUnit.Framework.TestCase("ellas van")]
-		[NUnit.Framework.TestCase("ustedes van")]
+
+		[NUnit.Framework.TestCase("yo grito")]
 		public void PresentTenseIr (string expected)
 		{
 			var results = new monarquia.EspanolGenerator().GetAll();
