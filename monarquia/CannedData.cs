18d376c3486eff543d2ce0f907fb36ad4255a394
fschwiet <fschwiet@gmail.com>
2015-04-15 17:51:56 -0500

    add more canned data

--- monarquia/CannedData.cs
+++ monarquia/CannedData.cs
@@ -9,12 +9,22 @@ namespace monarquia
 		{
 			AddVerbEnding ("beber", "leche");
 			AddVerbEnding ("comer", "fajitas");
+			AddVerbEnding ("dar", "un abrazo");
+			AddVerbEnding ("dar", "gritos");
+			//  AddVerbEnding ("dar", "la una");  ->  only clocks can strike one (to indicate a time)
 			AddVerbEnding ("hablar", "a la reportera");
 			AddVerbEnding ("hablar", "con él");
 			AddVerbEnding ("preparar", "la cena");
 			AddVerbEnding ("subir", "la escalera");
 			AddVerbEnding ("sumar", "la cuenta");
 			AddVerbEnding ("temer", "a los críticos");
+			AddVerbEnding ("tener", "frío");   // to be cold
+			AddVerbEnding ("tener", "hambre"); // to be hungry
+			AddVerbEnding ("tener", "miedo");  // to have fear
+			AddVerbEnding ("tener", "razón");  // to be right
+			AddVerbEnding ("tener", "sed");  // to be thirsty
+			AddVerbEnding ("tener", "prisa");  // to be in a hurry
+			AddVerbEnding ("tener", "culpa");  // to be at fault
 		}
 
 		void AddVerbEnding(string verbInfinitive, string ending) {
