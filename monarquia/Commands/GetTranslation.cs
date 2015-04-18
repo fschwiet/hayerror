570e141769b0dc05b9ec981092b9559ac1ca847a
fschwiet <fschwiet@gmail.com>
2015-04-18 18:53:38 -0500

    google translation adjusted so it won't look up recommended spellings (it would look up mal when given mes)

--- monarquia/Commands/GetTranslation.cs
+++ monarquia/Commands/GetTranslation.cs
@@ -54,6 +54,11 @@ namespace monarquia
 					client.Navigate ().GoToUrl ("about:blank");
 					client.Navigate ().GoToUrl ("https://translate.google.com/#es/en/" + WebUtility.UrlEncode(input));
 
+					var translateOriginalButton = client.FindElementsByCssSelector ("gt-revert-correct-message");
+
+					if (translateOriginalButton.Any ())
+						translateOriginalButton.First ().Click ();
+						
 					string result = null;
 					Polly.Policy.Handle<Exception> ().
 						WaitAndRetry(20, retryAttempt => TimeSpan.FromMilliseconds(200)).
